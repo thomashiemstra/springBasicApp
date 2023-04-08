0673d56e356aff2c4951cb63375b961c98fba4d1
Thomas <thomas-hiemstra@hotmail.com>
2023-04-08 11:19:22 +0200

    cleanup

--- src/main/kotlin/com/example/books/service/Auth0Service.kt
+++ src/main/kotlin/com/example/books/service/Auth0Service.kt
@@ -14,7 +14,7 @@ class Auth0Service(val auth0RestTemplate: RestTemplate, val auth0Properties: Aut
         val url = auth0Properties.issuer + "/oauth/token"
 
         val loginRequest = LoginRequest(
-                grant_type = GRANT_TYPE,
+                grant_type = PASSWORD_GRANT_TYPE,
                 username = request.username,
                 password = request.password,
                 audience = auth0Properties.audience,
@@ -24,22 +24,11 @@ class Auth0Service(val auth0RestTemplate: RestTemplate, val auth0Properties: Aut
 
         val response = auth0RestTemplate.postForObject(url, loginRequest, LoginResponse::class.java) ?: error("got no response from auth0")
 
-        //todo mapper
         return WebAuth0LoginResponse().jwtToken(response.access_token)
     }
 
-    //https://dev-xs1u5mnzrm83ke4o.us.auth0.com/oauth/token
-    //{
-    //  "grant_type":"password",
-    //  "username":"thomas-hiemstra@hotmail.com",
-    //  "password": "Admin@123",
-    //  "audience":"https://books/api",
-    //  "client_id":"0CM2y9a5ym9uE20hQvNp2ALr5lpRLr0E",
-    //  "client_secret":"3ohzRndl_YUjv9jqucHGBiyXu_ek3dH52CAur4aRHhXK8UL0cx5y8WXH96IjBnlf"
-    //}
-
     companion object {
-        const val GRANT_TYPE = "password"
+        const val PASSWORD_GRANT_TYPE = "password"
     }
 }
 
